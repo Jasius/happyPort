db4e47f8f692339382e8302a2d81f1edf07a41b2
Mindaugas Jasiulis <jasius@outlook.com>
2019-10-22 19:52:19 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# happyPort
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import "./styles.scss";
+import profileData from "./profile.json";
+
+const copyrightYear = `${
+  profileData.footer.initialWebsiteSetupYear
+} - ${new Date().getFullYear()}`;
+
+const currentProfile = `
+<div class="specialization">
+<div>Company ${profileData.specialization.currentCompany}</div>
+<div>Role ${profileData.specialization.role}</div>
+<div>Education ${profileData.specialization.eduInstitution}</div>
+<div>Programme ${profileData.specialization.eduProgramme}</div>
+<div>Hobbies ${profileData.specialization.hobbies}</div>
+</div>
+`;
+
+document.getElementById("app").innerHTML = `
+<header>
+<h1>Hi</h1>
+<h2>I am <span class="highlight">${profileData.profile.firstName},</span> a ${
+  profileData.specialization.role
+} based in <span class="highlight">Bristol</span></h2>
+<p class="primaryDescription">
+ ${profileData.primaryDescription}
+</p>
+<div class="specialization">
+${currentProfile}
+</div>
+</header>
+<footer>
+<span>${copyrightYear}</span>
+</footer>
+`;
