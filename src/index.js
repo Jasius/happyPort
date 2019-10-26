f7e278fff077fa44cbba33658b12c9d782701d99
Mindaugas Jasiulis <jasius@outlook.com>
2019-10-26 16:38:16 +0300

    switch cases

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import "./styles.scss";
 import profileData from "./profile.json";
 
-const copyrightYear = `${
-  profileData.footer.initialWebsiteSetupYear
-} - ${new Date().getFullYear()}`;
+const copyrightYear = () => {
+  if (profileData.footer.initialWebsiteSetupYear === new Date().getFullYear()) {
+    return `${profileData.footer.initialWebsiteSetupYear}`;
+  } else {
+    return `${
+      profileData.footer.initialWebsiteSetupYear
+    } - ${new Date().getFullYear()}`;
+  }
+};
 
 const currentProfile = `
 <div class="specialization">
@@ -11,9 +17,40 @@ const currentProfile = `
 <div>Role ${profileData.specialization.role}</div>
 <div>Education ${profileData.specialization.eduInstitution}</div>
 <div>Programme ${profileData.specialization.eduProgramme}</div>
-<div>Hobbies ${profileData.specialization.hobbies}</div>
+${profileData.specialization.hobbies ? `<div>Hobbies ${profileData.specialization.hobbies}</div>` : ''}
 </div>
 `;
+
+function socialProfiles() {
+  let existingProfiles = "",
+    profileIcon;
+  for (let socialLink in profileData.socialLinks) {
+    switch (socialLink) {
+      case "linkedin":
+        profileIcon = "linkedin-in";
+        break;
+      case "facebook":
+        profileIcon = "facebook-f";
+        break;
+      case "stackoverflow":
+        profileIcon = "stack-overflow";
+        break;
+      case "twitter":
+        profileIcon = "twitter";
+        break;
+      default:
+        profileIcon = "question-circle";
+    }
+    if (profileData.socialLinks[socialLink]) existingProfiles += `<a href="${
+      profileData.socialLinks[socialLink]
+    }"><i class="fab fa-${profileIcon}"></i></a>`;
+  }
+  return `
+  <div class="socialProfiles">
+  ${existingProfiles}
+  </div>
+  `;
+}
 
 document.getElementById("app").innerHTML = `
 <header>
@@ -27,9 +64,10 @@ document.getElementById("app").innerHTML = `
 <div class="specialization">
 ${currentProfile}
 </div>
+${socialProfiles()}
 <button onClick="">Contact me!</button>
 </header>
 <footer>
-<span>${copyrightYear}</span>
+<span>${copyrightYear()}</span>
 </footer>
 `;
