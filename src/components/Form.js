d41754c44a659e18785629f773bb4599b951c229
Mindaugas Jasiulis <jasius@outlook.com>
2020-07-19 23:09:25 +0300

    changed form padding

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,13 +3,13 @@ import React from 'react';
 function form () {
   return (
       <div className="mx-auto max-w-6xl p-12">
-          <div className="shadow-md flex flex-col md:flex-row justify-center">
+          <div className="shadow-md p-8 flex flex-col md:flex-row justify-center">
               <div className="md:w-1/2 max-w-md flex flex-col justify-center">
                   <div className="md:text-4xl text-xl font-black uppercase"><span className='bg-blue-500 text-white rounded px-1'>Seriously,</span> get in touch.</div>
                   <div className="text-xl mt-4">I am on the lookout for any kind of meaningful work. If you have something in mind or just want to say hi, drop me a line.</div>
               </div>
               <div className="md:w-1/2 flex justify-start mt-5 md:justify-end w-full md:w-1/2 ">
-                  <div className="flex-auto max-w-sm p-10 pb-20">
+                  <div className="flex-auto max-w-sm p-8 pb-20">
                       <div className="w-full">
                           <div className="font-bold h-6 mt-3 text-gray-600 text-xs leading-8 uppercase"><span className="text-red-400 mr-1">*</span> First Name</div>
                           <div className="my-2 bg-white p-1 flex border border-gray-200 rounded">  <input placeholder="John Doe" className="p-1 px-2 appearance-none outline-none w-full text-gray-800" />  </div>
