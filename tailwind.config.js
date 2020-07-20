a6c2d58a6f9e3f1802f07eac7aaa5c379064bcbf
Mindaugas Jasiulis <jasius@outlook.com>
2020-07-21 00:01:39 +0300

    added new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primaryBlue': '#2d4059',
+        'secondaryBlue': '#2d3748',
+      }
+    }
   },
   variants: {},
   plugins: [],
