f1a645575f2ac4cb1217701060c82725879672b8
Mindaugas Jasiulis <mindaugas.jasiulis@bazaarvoice.com>
2020-07-21 22:10:19 +0300

    added temp avatar

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header () {
         <section className="bg-white m-20">
             <div className="flex mx-auto max-w-screen-md">
                 <div className="lg:block hidden">
-                    <div className="h-48 w-48 bg-cover rounded-full border-solid border-2 border-green-600 flex items-center justify-center overflow-hidden" title="Profile picture" style={{ backgroundImage: `url('https://images-eds-ssl.xboxlive.com/image?url=wHwbXKif8cus8csoZ03RW3apWESZjav65Yncai8aRmVbSlZ3zqRpg1sdxEje_JmFQYY9q5JGf.Pd9sLQD6FJwRn1nuYA8hbE_i4.3p8sY4CaXnuE.AazJzwGhXEuUOW4TJA3BgJYBnUD7Be8CI3bp3oHAuC47ksD1MRHvXb8ymQ-&format=png')` }}></div>
+                    <div className="h-48 w-48 bg-cover rounded-full border-solid border-2 border-green-600 flex items-center justify-center overflow-hidden" title="Profile picture" style={{ backgroundImage: `url('https://jaska.dev/jaska.b4e4c830.png')` }}></div>
                 </div>
                 <div className="text-center ml-6">
                     <h2 className="text-3xl font-semibold text-gray-800">Hi, <span className="bg-blue-600 text-white rounded px-1">I’m Mindaugas</span>. Nice to meet you!</h2>
