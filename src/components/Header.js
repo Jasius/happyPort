cadba81329017023e38f3906514caf46c267340d
Mindaugas Jasiulis <mindaugas.jasiulis@bazaarvoice.com>
2020-07-21 22:23:03 +0300

    updated header description

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header () {
                 </div>
                 <div className="text-center ml-6">
                     <h2 className="text-3xl font-semibold text-gray-800">Hi, <span className="bg-blue-600 text-white rounded px-1">I’m Mindaugas</span>. Nice to meet you!</h2>
-                    <p className="text-gray-600 mt-4">I am a 23 years old ambitious front end developer who currently works in a flex role as internal tools developer, QA engineer and resides in a beautiful Vilnius city. Before going back to live in Vilnius I served as a conscript soldier in Marijampole's logistics battalion where I was assigned to SEP operator duties.
+                    <p className="text-gray-600 mt-4">I am a 23 years old ambitious front end developer who currently works in a flex role as internal tools developer, QA engineer and resides in a beautiful <a className="underline" href="https://govilnius.lt">Vilnius city.</a> Before going back to live in Vilnius <a className="underline" href="https://www.linkedin.com/posts/jasius_covid19-military-army-activity-6654753774902493184-bCm_">I served as a conscript soldier</a> in Marijampole's logistics battalion where I was assigned to SEP operator duties.
                     I have a drive to use skills and experience towards improving tools, services and communities such as Microsoft discord which has given me the chance to talk and gain knowledge, experience from Microsoft employees, prominent developers and journalists.
                     </p>
                 </div>
