d88c53cf6ac8a0ceeef143b1deac1de7dbd88f09
Mindaugas Jasiulis <mindaugas.jasiulis@bazaarvoice.com>
2020-07-27 22:44:16 +0300

    testimonials section title color change

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -4,7 +4,7 @@ import justeImg from '../assets/img/justeS.png'
 function Testimonials () {
     return (
         <div className="max-w-5xl px-6 mx-auto text-center py-20">
-            <h2 class="text-4xl font-semibold mb-10">Testimonials</h2>
+            <h2 class="text-4xl text-gray-800 font-semibold mb-10">Testimonials</h2>
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
                 <div class="max-w-md px-8 bg-white shadow-lg rounded-lg py-10">
                     <div class="flex justify-center">
