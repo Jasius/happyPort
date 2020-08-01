9ce7dfea4540ddda6e185bcd3d793ba6786002fc
Mindaugas Jasiulis <mindaugas.jasiulis@bazaarvoice.com>
2020-08-01 18:40:20 +0300

    json profile for testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,57 +1,42 @@
 import React from 'react';
-import justeImg from '../assets/img/justeS.png'
+import porfolioData from '../assets/portfolioData.json'
 
-function Testimonials () {
+const Testimonials = () => {
+    const testimonialTemplate = (testimonial) => <div className="max-w-md px-8 bg-white shadow-lg rounded-lg py-10 bg-opacity-25 testimonial-card">
+        {testimonial?.avatar?.length > 0 &&
+            <div className="flex justify-center">
+                <img className="w-20 h-20 object-cover rounded-full border-2 border-blue-600" loading="lazy" src={'assets/img/' + testimonial.avatar} alt={"Picture of " + testimonial.reviewer} />
+            </div>
+        }
+        <div>
+            <h2 className="text-gray-800 text-2xl font-semibold">
+                <svg xmlns="http://www.w3.org/2000/svg" className="inline align-baseline" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                    <path stroke="none" d="M0 0h24v24H0z" />
+                    <circle cx="12" cy="7" r="4" />
+                    <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+                </svg> {testimonial.reviewer}
+            </h2>
+            <h3 className="text-gray-800 text-1xl ">{testimonial.role}</h3>
+            <p className="mt-4 italic text-gray-600">{testimonial.testimonial}</p>
+        </div>
+    </div>
+    const [showRemaining, setShow] = React.useState(false);
+    const listTestimonials = porfolioData.testimonials.slice(0, 3).map((obj) => testimonialTemplate(obj));
+    const remainingTestimonials = porfolioData.testimonials.slice(3).map((obj) => testimonialTemplate(obj));
     return (
         <div className="max-w-5xl px-6 mx-auto text-center py-20">
-            <h2 class="text-4xl text-gray-800 font-semibold mb-2 cursor-default">Testimonials</h2>
-            <div class="mx-auto mb-10 w-16 border-b-3 border-gray-800 cursor-default"></div>
+            <h2 className="text-4xl text-gray-800 font-semibold mb-2 cursor-default">Testimonials</h2>
+            <div className="mx-auto mb-10 w-16 border-b-3 border-gray-800 cursor-default"></div>
             <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-                <div class="max-w-md px-8 bg-white shadow-lg rounded-lg py-10 bg-opacity-25">
-                    <div class="flex justify-center">
-                        <img class="w-20 h-20 object-cover rounded-full border-2 border-blue-600" src="https://jaska.dev/karolisN.d6da2a08.png" />
-                    </div>
-                    <div>
-                        <h2 class="text-gray-800 text-2xl font-semibold"><svg xmlns="http://www.w3.org/2000/svg" class="inline align-baseline" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" />
-                            <circle cx="12" cy="7" r="4" />
-                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                        </svg> Karolis N.</h2>
-                        <h3 class="text-gray-800 text-1xl ">Delivery Ops Lead</h3>
-                        <p class="mt-4 italic text-gray-600">Thanks for all the improvements, amazing work on internal tool and constant technical help!</p>
-                    </div>
-                </div>
-                <div class="max-w-md px-8 bg-white shadow-lg rounded-lg py-10 bg-opacity-25">
-                    <div class="flex justify-center">
-                        <img class="w-20 h-20 object-cover rounded-full border-2 border-blue-600" src={justeImg} />
-                    </div>
-                    <div>
-                        <h2 class="text-gray-800 text-2xl font-semibold"><svg xmlns="http://www.w3.org/2000/svg" class="inline align-baseline" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" />
-                            <circle cx="12" cy="7" r="4" />
-                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                        </svg> Juste S.</h2>
-                        <h3 class="text-gray-800 text-1xl ">Delivery Ops Manager</h3>
-                        <p class="mt-4 italic text-gray-600">Thank you for your endless ideas of innovation and valuable input for every initiative you come up with or stumble upon, but also for keeping the team spirits up with relentless humour and OC memes! What a gem of a personality you are!</p>
-                    </div>
-                </div>
-                <div class="max-w-md px-8 bg-white shadow-lg rounded-lg py-10 bg-opacity-25">
-                    <div class="flex justify-center">
-                        <img class="w-20 h-20 object-cover rounded-full border-2 border-blue-600" src="https://jaska.dev/tomasS.0a6948d5.jpg" />
-                    </div>
-                    <div>
-                        <h2 class="text-gray-800 text-2xl font-semibold"><svg xmlns="http://www.w3.org/2000/svg" class="inline align-baseline" width="20" height="20" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" />
-                            <circle cx="12" cy="7" r="4" />
-                            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                        </svg> Tomas S.</h2>
-                        <h3 class="text-gray-800 text-1xl ">Implementation Manager</h3>
-                        <p class="mt-4 italic text-gray-600">Thank you for your help on building QA Automation tool.</p>
-                    </div>
-                </div>
+                {listTestimonials}
+                {showRemaining ? (
+                    remainingTestimonials
+                ) : null}
             </div>
+            {porfolioData.testimonials.length > 3 && <button onClick={() => setShow(!showRemaining)} className="w-32 font-medium m-6 py-2 px-4 text-white bg-blue-600 rounded text-lg tr-mt hover:bg-blue-400 ease-in-out duration-200">Show {!showRemaining ? 'more' : 'less'}</button>}
         </div>
     );
+
 }
 
 export default Testimonials;
